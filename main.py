ee1852cc01f0cfc7af2f8c619240a3296718db4b
Kelly-Kurowski <k.kurowski@students.uu.nl>
2024-09-18 10:44:38 +0200

    Commit for Windows

--- main.py
+++ main.py
@@ -3,20 +3,18 @@ import time
 import pyautogui
 from datetime import datetime
 
-# Define the recipient's phone number and message
-phone_numbers = ["+31 6 28401213", "+31 6 28401213"]  # Include country code, e.g., +1 for the US
-message = "Vergeet niet de survey in te vullen!"
+# Define the phone numbers and the message
+phone_numbers = ["+31 6 28401213"]  # Include country code, e.g., +31 6 12345678
+message = "Dit is een automatisch gegenereerd bericht: vergeet a.u.b. niet de survey in te vullen vanavond!"
 
 # Get the current time and calculate the time to send the message
 now = datetime.now()
-send_hour = 9  # 17:00 or 5:00 PM
-send_minute = 56  # Set the minute for the message
+send_hour = 9  # for example: 9 AM = 9 and 5 PM = 17
+send_minute = 5  # Set the minute for the message, never add a 0 before the number
 
+# Send the message
 for number in phone_numbers:
     kit.sendwhatmsg(number, message, send_hour, send_minute)
-    send_minute += 1
-    pyautogui.hotkey('ctrl', 'w')
-
-    # with pyautogui.hold(['command']):
-    #     time.sleep(1)
-    #     pyautogui.press('w')
+    time.sleep(15) # Recovery time has to be 15 seconds
+    send_minute += 1 # Next message needs to be sent a minute after start time
+    pyautogui.hotkey('ctrl', 'w') # Close window
